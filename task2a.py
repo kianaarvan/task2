e0fc2da57d7336263651642b9f647afa89b53d3c
kianaarvan <148455828+kianaarvan@users.noreply.github.com>
2023-10-19 18:23:04 +0330

    Add files via upload

--- task2a.py
+++ task2a.py
@@ -2,7 +2,7 @@ a=int(input('enter number a:'))
 b=int(input('enter number b:'))
 c=int(input('enter number c:'))
 c=a+b
-if c<<a+b and b<<a+c and a<<b+c :
+if c<a+b and b<a+c and a<b+c :
     print('we can draw a triangle')
 else :
     print('we cant draw a triangle')
